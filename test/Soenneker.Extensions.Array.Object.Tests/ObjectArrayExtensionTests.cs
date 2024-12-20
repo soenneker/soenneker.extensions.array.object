543891c07b9394aec70afa8a596afadbaddf26ab
Jake Soenneker <jake@soeneker.com>
2024-12-20 17:34:19 +0000

    Tests

---
+++ test/Soenneker.Extensions.Array.Object.Tests/ObjectArrayExtensionTests.cs
@@ -0,0 +1,13 @@
+using Soenneker.Tests.Unit;
+using Xunit;
+
+namespace Soenneker.Extensions.Array.Object.Tests;
+
+public class ObjectArrayExtensionTests : UnitTest
+{
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
