5485c489683fe2ca36e0a9180629d62acc3e647a
Jake Soenneker <jake@soenneker.com>
2026-01-11 20:26:31 -0600

    readonly impl

--- src/ObjectArrayExtension.cs
+++ src/ObjectArrayExtension.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Diagnostics.Contracts;
 using System.Runtime.CompilerServices;
+using Soenneker.Extensions.Spans.Readonly.Objects;
 
 namespace Soenneker.Extensions.Array.Object;
 
@@ -15,16 +16,5 @@ public static class ObjectArrayExtension
     /// <param name="objects">The array of objects to convert.</param>
     /// <returns>An array of Type objects representing the types of the input objects.</returns>
     [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static Type[] ToTypes(this object[] objects)
-    {
-        int length = objects.Length;
-        var parameterTypes = new Type[length];
-
-        for (var i = 0; i < length; i++)
-        {
-            parameterTypes[i] = objects[i].GetType();
-        }
-
-        return parameterTypes;
-    }
+    public static Type[] ToTypes(this object[] objects) => ((ReadOnlySpan<object>) objects).ToTypes();
 }
